a16e576a8bfbf3a8f86db7f15ff9e1c6d606d30c
BobRazoswki <bobongithub@gmail.com>
2014-09-01 11:41:09 +0100

    add total rooms

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,14 @@
 class Property < ActiveRecord::Base
 
 	has_many :rooms
+
+	def total_rooms
+	end
+
+	def total_rooms=(number)
+		1.upto(number.to_i) do |n|
+			Room.create(number: n, property_id: self.id)
+		end 
+	end
+
 end
--- spec/features/room_feature_spec.rb
+++ spec/features/room_feature_spec.rb
@@ -23,5 +23,16 @@ describe 'Room' do
 		expect(page).to have_content 'Small room'
 	end
 
+	it 'can create a property' do
+		visit '/properties/new'
+		fill_in :property_title, with: "Great flat near old street"
+		fill_in :property_address, with: "25 city road"
+		fill_in :property_postcode, with: "EC1Y 1AA"
+		fill_in :property_city, with: "London"
+		fill_in :property_total_rooms, with: "5"
+		fill_in :property_description, with: "Makers Academy"
+		click_button 'Submit'
+		expect(page).to have_content("Great flat near old street")
+	end
 
 end
