e821029b7f61b7634c5a22c71e417829d85211ed
BobRazoswki <bobongithub@gmail.com>
2014-09-01 10:45:31 +0100

    add user and merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140901084151) do
+ActiveRecord::Schema.define(version: 20140901092958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +25,6 @@ ActiveRecord::Schema.define(version: 20140901084151) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "rooms", force: true do |t|
     t.integer  "number"
