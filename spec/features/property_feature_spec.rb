dce1523d9abf0165ae65471bb04929bc8fa5754d
BobRazoswki <bobongithub@gmail.com>
2014-09-01 09:19:09 +0100

    add form for property test

--- spec/features/property_feature_spec.rb
+++ spec/features/property_feature_spec.rb
@@ -11,4 +11,15 @@ describe 'Property' do
 		expect(page).to have_content 'Great flat near old street'
 	end
 
+	it 'can create a property' do
+		visit '/properties/new'
+		fill_in :title, with: "Great flat near old street"
+		fill_in :address, with: "25 city road"
+		fill_in :postcode, with: "EC1Y 1AA"
+		fill_in :city, with: "London"
+		fill_in :description, with: "Makers Academy"
+		click_button 'Submit'
+		expect(page).to have_content("Great flat near old street")
+	end
+
 end
