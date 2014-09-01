4a261cfd6a85c094de712ca02e0ebeea3d6e09b3
marcoaam <marco.army@gmail.com>
2014-09-01 09:32:33 +0100

    create a property form

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,11 +1,15 @@
 class PropertiesController < ApplicationController
 
-	def new
-		@property = Property.new
-	end
-
 	def index
 		@properties = Property.all
 	end
+
+	def new
+		@property = Property.new
+	end
 	
+	def create
+		Property.create(params[:property].permit(:title, :address, :postcode, :city, :description))
+		redirect_to '/properties'
+	end
 end
--- spec/features/property_feature_spec.rb
+++ spec/features/property_feature_spec.rb
@@ -13,11 +13,11 @@ describe 'Property' do
 
 	it 'can create a property' do
 		visit '/properties/new'
-		fill_in :title, with: "Great flat near old street"
-		fill_in :address, with: "25 city road"
-		fill_in :postcode, with: "EC1Y 1AA"
-		fill_in :city, with: "London"
-		fill_in :description, with: "Makers Academy"
+		fill_in :property_title, with: "Great flat near old street"
+		fill_in :property_address, with: "25 city road"
+		fill_in :property_postcode, with: "EC1Y 1AA"
+		fill_in :property_city, with: "London"
+		fill_in :property_description, with: "Makers Academy"
 		click_button 'Submit'
 		expect(page).to have_content("Great flat near old street")
 	end
