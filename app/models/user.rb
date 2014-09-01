3bf596f833afc98f71bcf07e17316d9ccd466b1e
jeantroiani <digitalguest@gmail.com>
2014-09-01 10:34:43 +0100

    added user model, username column to users, and users can use username to login

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  
+  attr_accessor :login
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+      else
+        where(conditions).first
+      end
+  end
+
+
+end
