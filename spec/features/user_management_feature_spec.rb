86a5fc261e638b7bd008d688547f246c3c8f7a7d
marcoaam <marco.army@gmail.com>
2014-09-03 18:34:47 +0100

    modals for sign in and sign up implemented

---
+++ spec/features/user_management_feature_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+describe 'User menu display' do
+
+	context 'if logged out' do
+
+		it 'display sign in' do
+			visit('/')
+			expect(page).to have_content('Sign in')
+		end
+
+		it 'display sign up' do
+			visit('/')
+			expect(page).to have_content('Sign up')
+		end
+
+	end
+
+	context 'if logged in' do
+
+		before(:each) do
+			alex = create(:user)
+			login_as alex
+		end
+	
+		it 'display sign out' do
+			visit('/')
+			expect(page).to have_content('Sign out')
+		end
+
+	end
+
+	context 'js enabled' do
+
+		before(:each) do
+			create(:user)
+		end
+
+		it 'can sign up', js: true do
+			visit ('/')
+			click_button 'Sign up'
+			within("#sign_up_modal") do
+				fill_in ('user[username]'), with: 'marco', match: :prefer_exact
+				fill_in ('user[email]'), with: 'm@m.com', match: :prefer_exact
+				fill_in ('user[password]'), with: '12345678', match: :prefer_exact
+				fill_in ('user[password_confirmation]'), with: '12345678', match: :prefer_exact
+				click_button 'Subscribe'
+			end
+			expect(page).to have_content 'Welcome! You have signed up successfully.'
+		end
+
+		it 'can sign in', js: true do
+			visit ('/')
+			click_button 'Sign in'
+			within("#sign_in_modal") do
+				fill_in ('user[login]'), with: 'John', match: :prefer_exact
+				fill_in ('user[password]'), with: '12345678', match: :prefer_exact
+				click_button 'login'
+			end
+			expect(page).to have_content 'Signed in successfully.'
+		end
+
+	end
+
+end
