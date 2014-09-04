1270fb2a8f1d603ce6c53ccb67758a5234c49e74
BobRazoswki <bobongithub@gmail.com>
2014-09-04 12:56:49 +0100

    css v6

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
+//show the right room number on properties/id page
+
+
+$("li [role='tab']").on('click', function() {
+
+	var dest = $(this).attr('data-room');
+  showTab(dest);
+
+});
+
+function showTab(tab) {
+	$("#room-tab li").hide();
+  $("#room-tab [data-room='" + tab + "']").show();
+}
+
+
