35540cdf8d1bc25fcad492fae5702eb613e832a1
jeantroiani <digitalguest@gmail.com>
2014-09-11 19:02:44 +0100

    Added pictures reviews and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,26 +11,47 @@ Review.destroy_all
 Property.destroy_all
 Picture.destroy_all
 
-titles = ["Gorgeous large modern flat - Angel Shoreditch area"]
+titles = ["Gorgeous large modern flat - Angel Shoreditch area","Double Bedroom in Old Street","Room in Amazing New Flat in beautiful London","Double rooms/Lounge & Balcony", "Nice Double Mins Away from Shoreditch Overground", "3 Bedroom Fully Furnished Flat"]
 
 descriptions = ["The flat is a 1, 000 sq ft 3 bedroom flat, there are two bathrooms (including an en-suite for the master room), a large eat in kitchen, and a sizeable lounge. The flat obviously has all mod cons, broadband, sky etc.
-THe area is AMAZING in so many respects! Transport links are fantastic, you have 3 bus links on the doorstep (76 to Waterloo, 21 to Lewisham via City, and 141 to London bridge) all buses take you to Old Street station on the northern line in less than 5 mins, or it's an easy 15 minute walk door to door."]
+The area is AMAZING in so many respects! Transport links are fantastic, you have 3 bus links on the doorstep (76 to Waterloo, 21 to Lewisham via City, and 141 to London bridge) all buses take you to Old Street station on the northern line in less than 5 mins, or it's an easy 15 minute walk door to door.","3 minute walk from Old Street Station and a further 10 minute walk to more than 4 other train stations. At the moment there is just one other person including myself, I am a student and so do not spend of my time at home, the other tenant is working. We share most of the eating expenses but if you want to only pay for yourself that will be just fine. The room is a double bedroom and used to belong to a student who moved out only about 1 week ago.","The flat is pretty much brand new, is fully furnished with quality new gear and is great for sharing. Your room is large, has a double bed and best of all you have your own bathroom, which is very rare for an flat this nicely decked out and this close to the city.","a lovely bedroom property situated between Angel and Kings Cross St Pancras tube station. Convenient for the shops, bars and restaurants of vibrant Upper Street, with easy access to the City, Shoreditch and the West Ends shopping district.
+The property comprises of two good-sized double bedrooms & the third with spacious lounge, which leads out onto a private balcony overlooking the communal park, a good size modern fully fitted kitchen with appliances, and a tiled bathroom and three separate storage spaces.", " Fabulous double room available in a three Bedroom Duplex Flat with its own balcony and huge storage space, located on the 3rd floor of a well-maintained and nice building in between Kings Cross and Caledonian Road Stations. THE FLAT: The flat is a 3-bedroom duplex flat, although there is not a living room, the kitchen is fully equipped with dining table, hot water, washing machine, large fridge, oven, kettle, microwave, toaster and pretty much anything you need to have a pleasant stay. There is full heating system that works great, 24/7 hot water; separate toilet and bathroom. The flat has beautiful laminate floor throughout.", "Cose to transport, five minutes Hoxton station 15 minutes Old St station Convenient for Hoxton St Market and the Bars, Restaurants and Night Life of Hoxton, Shoreditch"]
 
+address = ['341 Upper St,London,N1 0PB','25 City Rd,London,EC1Y 1AA','91 City Road,London,EC1Y 1AF','1st Floor Leonard St,London,EC2A','87A Great Eastern Street,London,EC2A','127 91 City Road,London,EC1Y','Weymouth Terrace,London,E2','The South Penthouse,London,EC2Y','Duncan Terrace ,London,N1']
+
+tenant_reviews = ["Had a great time in this flat.", "The place is very central and close to various amenities. I had a room with my own private bathroom and a shared kitchen where I could cook. The place is clean and well kept. I also high speed internet access.","Thank you, for making my stay so comfortable x","Very nice flatmate! The atmosphere is warm and friendly","He is an easygoing, willing and sociable","Fue de gran ayuda, siempre pendiente de lo que ncesitaba",'Ce proprio m\'a tant comble que je le recommande a toute celles ou tout ceux qui ont besoin de quelqu un sur qui compter']
+property_reviews = ['The room we had was really a small small flat.','The management was very helpful.','This accommodation is the best location to stay in Old Street, we were in a quiet place and very close to the bus stops,','I really enjoyed my stay at this quiet homestay in the heart of London, I would recommend it especially for students','Mi estadia durante estos tres meses fue increible, el cuarto estaba impecable','La propiedad esta en perfecto estado, muy bien cuidada. 100% recomendada']
 
 marco = User.create(username: "marco", email: "marco@test.com", password: "12345678", password_confirmation: "12345678")
-jean = User.create(username: "jean", email: "jean@test.com", password: "12345678", password_confirmation: "12345678")
-alex = User.create(username: "alex", email: "alex@test.com", password: "12345678", password_confirmation: "12345678")
+jean =  User.create(username: "jean",  email: "jean@test.com",  password: "12345678", password_confirmation: "12345678")
+alex =  User.create(username: "alex",  email: "alex@test.com",  password: "12345678", password_confirmation: "12345678")
+marco.pictures.create(image: File.new(Rails.root.join('public/images/marco.jpg')))
+jean.pictures.create(image: File.new(Rails.root.join('public/images/jean.jpg')))
+alex.pictures.create(image: File.new(Rails.root.join('public/images/alex.jpg')))
 
-picture_1 = File.new(Rails.root.join('public/images/room1.jpg'))
-picture_2 = File.new(Rails.root.join('public/images/outside1.jpg'))
+room_1 = File.new(Rails.root.join('public/images/room_1.jpg'))
+outside_1 = File.new(Rails.root.join('public/images/outside_1.jpg'))
+room_2 = File.new(Rails.root.join('public/images/room_2.jpg'))
+outside_2 = File.new(Rails.root.join('public/images/outside_2.jpg'))
+room_3 = File.new(Rails.root.join('public/images/room_3.jpg'))
+outside_3 = File.new(Rails.root.join('public/images/outside_3.jpg'))
+rooms = [room_1,room_2,room_3]  
+outsides = [outside_1,outside_2,outside_3]
 
-pictures = [picture_1, picture_2]
+users = [marco, jean, alex]
 
-users = [marco, jean]
 
-1.upto(3) do |n|
-  property = Property.create(title: titles.sample, description: descriptions.sample, address: '341 Upper St', city: 'London', postcode: 'N1 0PB')
+1.upto(10) do |n|
+  property = Property.create(title: titles.sample, description: descriptions.sample, address: address.sample.split(",")[0] , city: address.sample.split(",")[1], postcode: address.sample.split(",")[2], total_rooms:[*1..3].sample.to_s)
   users.sample.properties << property
-  2.times { property.pictures.create(image: pictures.sample) }
-  users.sample.reviews << Review.create(thoughts: 'Had a great time in this flat. The place is very central and close to various amenities. I had a room with my own private bathroom and a shared kitchen where I could cook. The place is clean and well kept. I also high speed internet access.', rating: 5, user_id: users.sample.id)
+  property.pictures.create(image: outsides.sample)
+  property.pictures.create(image: rooms.sample)
+  3.times { users.sample.reviews << Review.create(thoughts: tenant_reviews.sample, rating: [*1..5].sample, user_id: users.sample.id) }
+  5.times{ property.reviews << Review.create(thoughts: tenant_reviews.sample, rating: [*1..5].sample, user_id: users.sample.id) }
+	puts "#{n} uploaded"
 end
+
+
+
+
+
